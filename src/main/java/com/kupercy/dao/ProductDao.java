3c26d22ef27b3efb61ed9d265e38a279f105dbbd
guxiansheng <percyku19@gmail.com>
2022-05-02 17:12:00 +0800

    商品功能 - 實作查詢商品

---
+++ src/main/java/com/kupercy/dao/ProductDao.java
@@ -0,0 +1,9 @@
+package com.kupercy.dao;
+
+import com.kupercy.model.Product;
+
+public interface ProductDao {
+
+
+    Product getProductById(Integer productId);
+}
---
+++ src/main/java/com/kupercy/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.kupercy.service;
+
+import com.kupercy.model.Product;
+
+public interface ProductService {
+
+    Product getProductById(Integer productId);
+}
