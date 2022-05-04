bacbed590056f5329bbbd58860f5537855aaa8d5
guxiansheng <percyku19@gmail.com>
2022-05-04 23:01:33 +0800

    商品功能 - 實作修改商品

--- src/main/java/com/kupercy/dao/ProductDao.java
+++ src/main/java/com/kupercy/dao/ProductDao.java
@@ -9,4 +9,7 @@ public interface ProductDao {
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
+
+    void updateProduct(Integer productId,ProductRequest productRequest);
+
 }
--- src/main/java/com/kupercy/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/kupercy/dao/impl/ProductDaoImpl.java
@@ -43,13 +43,13 @@ public class ProductDaoImpl implements ProductDao {
     @Override
     public Integer createProduct(ProductRequest productRequest) {
         String sql ="INSERT INTO product( product_name, category, image_url, price, stock, description, created_date, last_modified_date)"+
-                "VALUES(:productName, :category, :imageUrl, :prices, :stock, :description, :createdDate, :lastModifiedDate)";
+                "VALUES(:productName, :category, :imageUrl, :price, :stock, :description, :createdDate, :lastModifiedDate)";
 
         Map<String, Object>map =new HashMap<>();
         map.put("productName",productRequest.getProductName());
         map.put("category",productRequest.getCategory().toString());
         map.put("imageUrl",productRequest.getImageUrl());
-        map.put("prices",productRequest.getPrice());
+        map.put("price",productRequest.getPrice());
         map.put("stock",productRequest.getStock());
         map.put("description",productRequest.getDescription());
 
@@ -64,5 +64,27 @@ public class ProductDaoImpl implements ProductDao {
         int productId =keyHolder.getKey().intValue();
         return productId;
 
+    }
+
+    @Override
+    public void updateProduct(Integer productId, ProductRequest productRequest) {
+        String sql ="UPDATE product SET product_name = :productName, category= :category, image_url = :imageUrl, " +
+                "price = :price, stock = :stock, description=:description ,last_Modified_date = :lastModifiedDate "+
+                "WHERE product_id = :productId";
+
+
+        Map<String, Object>map =new HashMap<>();
+        map.put("productId",productId);
+
+        map.put("productName",productRequest.getProductName());
+        map.put("category",productRequest.getCategory().toString());
+        map.put("imageUrl",productRequest.getImageUrl());
+        map.put("price",productRequest.getPrice());
+        map.put("stock",productRequest.getStock());
+        map.put("description",productRequest.getDescription());
+
+        map.put("lastModifiedDate",new Date());
+
+        namedParameterJdbcTemplate.update(sql,map);
     }
 }
