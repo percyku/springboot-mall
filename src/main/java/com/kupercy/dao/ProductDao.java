bcf4e16266201944d8f152c7986127ab4350b7b8
guxiansheng <percyku19@gmail.com>
2022-05-05 19:36:39 +0800

    商品功能 - 實作查詢商品列表 (基礎實作)

--- src/main/java/com/kupercy/dao/ProductDao.java
+++ src/main/java/com/kupercy/dao/ProductDao.java
@@ -3,8 +3,11 @@ package com.kupercy.dao;
 import com.kupercy.dto.ProductRequest;
 import com.kupercy.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
 
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- src/main/java/com/kupercy/service/ProductService.java
+++ src/main/java/com/kupercy/service/ProductService.java
@@ -3,7 +3,11 @@ package com.kupercy.service;
 import com.kupercy.dto.ProductRequest;
 import com.kupercy.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
