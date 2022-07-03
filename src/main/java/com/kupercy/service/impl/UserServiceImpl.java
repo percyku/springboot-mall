6684e9f1082ce3307a3e40c3c274315fa1fa7335
guxiansheng <percyku19@gmail.com>
2022-07-03 13:46:39 +0800

    帳號功能 - 檢查註冊的email, 隱藏返回的密碼

--- src/main/java/com/kupercy/service/impl/UserServiceImpl.java
+++ src/main/java/com/kupercy/service/impl/UserServiceImpl.java
@@ -4,11 +4,16 @@ import com.kupercy.dao.UserDao;
 import com.kupercy.dto.UserRegisterRequest;
 import com.kupercy.model.User;
 import com.kupercy.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired
     private UserDao userDao;
 
@@ -20,6 +25,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        //check register email
+        User user =userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if(user != null){
+            log.warn("This email {} had been registered.{}",userRegisterRequest.getEmail(),"USER");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        //create user account
         return userDao.createUser(userRegisterRequest);
     }
 }
